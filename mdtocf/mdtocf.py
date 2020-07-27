f93a5efbd43a969a089557f59513f03bd0d49943
Ben Wart <ben.wart@opensesame.com>
2020-07-27 14:35:14 -0700

    support passing in required fields through envvars
    allows github actions to function properly when called with direct docker link vs repo link with action file

--- mdtocf/mdtocf.py
+++ mdtocf/mdtocf.py
@@ -4,33 +4,42 @@ This script convert markdown pages and publish them to confluence pages
 
 """
 import argparse
+import os
 from .ConfluencePublisher import ConfluencePublisher
+
+
+def environ_or_required(key):
+    return (
+        {'default': os.environ.get(key)} if os.environ.get(key)
+        else {'required': True}
+    )
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--confluenceUsername',
-                        required=True,
-                        help='e.g. "example@example.com"')
+                        help='e.g. "example@example.com"',
+                        **environ_or_required('INPUT_CONFLUENCE-USERNAME'))
     parser.add_argument('--confluenceApiToken',
-                        required=True,
-                        help='e.g. "a87D98AfDsf98dsf7AdsNfaa2"')
+                        help='e.g. "a87D98AfDsf98dsf7AdsNfaa2"',
+                        **environ_or_required('INPUT_CONFLUENCE-API-TOKEN'))
     parser.add_argument('--confluenceUrl',
-                        required=True,
-                        help='e.g. "https://example.jira.com/"')
+                        help='e.g. "https://example.jira.com/"',
+                        **environ_or_required('INPUT_CONFLUENCE-URL'))
     parser.add_argument('--confluenceSpace',
-                        required=True,
-                        help='e.g. ~989819389 (Personal Space), 78712486')
+                        help='e.g. ~989819389 (Personal Space), 78712486',
+                        **environ_or_required('INPUT_CONFLUENCE-SPACE'))
     parser.add_argument('--confluenceParentPageId',
-                        required=True,
-                        help='e.g. "Page Information: ?pageId=1650458860')
+                        help='e.g. "Page Information: ?pageId=1650458860',
+                        **environ_or_required('INPUT_CONFLUENCE-PARENT-PAGEID')
+                        )
     parser.add_argument('--confluencePageTitlePrefix',
                         required=False,
                         help='e.g. "[MyPrefix] "',
                         default='')
     parser.add_argument('--markdownDir',
-                        required=True,
-                        help='e.g. "../mydocs"')
+                        help='e.g. "../mydocs"',
+                        **environ_or_required('INPUT_MARKDOWN-DIR'))
     parser.add_argument('--dbPath',
                         required=False,
                         help='e.g. "./dbs/mydocs.db"',
